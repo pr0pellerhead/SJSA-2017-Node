ef076de179154d855a0e556fc93d7e76554d4bb9
Bojan Gavrovski <bojan.gavrovski@gmail.com>
2018-02-26 22:05:44 +0100

    Working with files. Building a basic web server

---
+++ c3/index.js
@@ -0,0 +1,37 @@
+var fs = require('fs');
+var http = require('http');
+
+// fs.writeFile('poraka.txt', 'NodeJs е најдобар јазик на свет... и пошироко...', (err) => {
+//     if (err) throw err;
+//     console.log('The file has been saved!');
+// });
+
+// fs.appendFile('poraka.txt', 'nova linija', (err) => {
+//     if (err) throw err;
+//     console.log('The "data to append" was appended to file!');
+// });
+
+// fs.readFile('users.json', 'utf8', (err, data) => {
+//     // console.log(data);
+//     var jsonData = JSON.parse(data);
+//     // console.log(jsonData);
+
+//     jsonData.forEach((user, i) => {
+//         console.log('Name: ' + user.name + '\t' + 'Lastname: ' + user.lastname + '\t' + 'Email: ' + user.email + '\t' + 'Password: ' + user.password);
+//     });
+// });
+
+http.createServer(function (request, response) { // req, res | r, w
+    if(request.url === '/hello'){
+        response.writeHead(200, 'OK');
+        response.end('Hello World!');
+    } else {
+        response.writeHead(404, 'Not Found');
+        response.end('Page not found...');
+    }
+}).listen(3000); // 1337
+
+// http://localhost:3000/hello -> HELLO WORLD
+// http://localhost:3000/
+// http://localhost:3000/ruwyeiurywe
+// http://localhost:3000/gc764d782ho876 // 404 NOT FOUND
