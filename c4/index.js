255c4c9a4290a8aa0f4e7c5299c1c09f0fd18767
Bojan Gavrovski <bojan.gavrovski@gmail.com>
2018-03-07 22:02:04 +0100

    Full featured API service from scratch with in memory datastore

--- c4/index.js
+++ c4/index.js
@@ -1,5 +1,8 @@
 var http = require('http');
 
+// users datastore
+var DATA = [];
+
 function URLToRegex(url) {
     var output = url.replace(new RegExp('/', 'g'), '\\/')
         .replace(new RegExp('{num}', 'g'), '[0-9]+')
@@ -11,31 +14,110 @@ function URLToRegex(url) {
 
 var routes = {GET: [], POST: [], PATCH: [], PUT: [], DELETE: []};
 
+// Get all users from the datastore
 routes.GET['/users'] = function (req, res) {
     res.writeHead(200, 'OK');
-    res.end('GET ALL USERS');
+    res.end(JSON.stringify(DATA));
 };
 
+// Get user by ID
 routes.GET['/users/{num}'] = function (req, res) {
-    res.writeHead(200, 'OK');
-    res.end('GET SINGLE USER');
+    var url = req.url.split('/');
+    var id = parseInt(url[url.length - 1]);
+
+    if(DATA[id] != undefined){
+        res.writeHead(200, 'OK');
+        res.end(JSON.stringify(DATA[id]));
+    } else {
+        res.writeHead(404, 'Not Found');
+        res.end('Not Found');
+    }
 };
 
+// Create/add new user to the datastore
 routes.POST['/users'] = function (req, res) {
-    res.writeHead(200, 'OK');
-    res.end('POST USERS');
+    var d = '';
+
+    req.on('data', function(data){
+        d += data;
+    });
+
+    req.on('end', function(){
+        DATA.push(JSON.parse(d));
+        res.writeHead(200, 'OK');
+        res.end('POST USERS');
+    });
 };
+
+// Delete user from the datastore
+routes.DELETE['/users/{num}'] = function(req, res){
+    var url = req.url.split('/');
+    var id = parseInt(url[url.length - 1]);
+
+    if(DATA[id] != undefined){
+        // delete DATA[id];
+        DATA.splice(id, 1);
+        res.writeHead(200, 'OK');
+        res.end('');
+    } else {
+        res.writeHead(404, 'Not Found');
+        res.end('Not Found');
+    }
+}
+
+// Update complete user data
+routes.PUT['/users/{num}'] = function (req, res) {
+    var url = req.url.split('/');
+    var id = parseInt(url[url.length - 1]);
+    var d = '';
+
+    req.on('data', function (data) {
+        d += data;
+    });
+
+    req.on('end', function () {
+        if (DATA[id] != undefined) {
+            DATA[id] = JSON.parse(d);
+            res.writeHead(200, 'OK');
+            res.end('POST USERS');
+        } else {
+            res.writeHead(404, 'Not Found');
+            res.end('Not Found');
+        }
+    });
+}
+
+// Update part od user's data
+routes.PATCH['/users/{num}/(firstname|lastname|birtdate|password|email)'] = function (req, res) {
+    var d = '';
+    var url = req.url.split('/');
+
+    var id = parseInt(url[url.length - 2]);
+    var property = url[url.length - 1];
+
+    req.on('data', function (data) {
+        d += data;
+    });
+
+    req.on('end', function () {
+        if (DATA[id] != undefined && JSON.parse(d)[property] != undefined) {
+            DATA[id][property] = JSON.parse(d)[property];
+            res.writeHead(200, 'OK');
+            res.end('');
+        } else {
+            res.writeHead(404, 'Not Found');
+            res.end('Not Found');
+        }
+    });
+}
 
 http.createServer(function (req, res) {
-    
     var error = true;
 
     for(i in routes[req.method]){
-
         var regex = new RegExp(URLToRegex(i));
 
         if (regex.test(req.url)) {
-
             if (typeof routes[req.method][i] == 'function'){
                 error = false;
                 routes[req.method][i](req, res);
@@ -51,5 +133,4 @@ http.createServer(function (req, res) {
         res.writeHead(404, 'Not Found');
         res.end('Not Found');
     }
-
 }).listen(3000);
